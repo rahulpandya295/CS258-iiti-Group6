246ededf3da051928ca18478cfc8f0dbd33f1e86
izanami79 <r4.0radhe79@gmail.com>
2014-02-27 13:50:12 +0530

    Logout

---
+++ logout.php
@@ -0,0 +1,13 @@
+<?php 
+	session_start();
+	if(!isset($_SESSION['isLoggedIn'])){
+			session_destroy();
+			header("location: Login_form.php");
+			die();
+		}
+	session_destroy();
+	session_start();
+	$_SESSION['isLoggedOut']=1;
+	header('location: Login_form.php?status='.urlencode(base64_encode("You have been successfully logged out!")));
+	//this msg will be sent through URL in encrypted form
+?>
