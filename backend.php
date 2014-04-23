5de61b843bb310e76a337fda4e10e55b05bd5de1
Vishwas Jain <vishwasj8@gmail.com>
2014-04-23 15:20:55 +0530

    final backend.php
    consists of all the headers required in mail function.

---
+++ backend.php
@@ -0,0 +1,56 @@
+<?php
+function mail_attachment($filename, $path, $mailto, $from_mail, $from_name, $replyto, $subject, $message) {
+    $file = $path.$filename;
+    $file_size = filesize($file);
+    $handle = fopen($file, "r");
+    $content = fread($handle, $file_size);
+    fclose($handle);
+    $content = chunk_split(base64_encode($content));
+    $uid = md5(uniqid(time()));
+    $name = basename($file);
+    $header = "From: ".$from_name." <".$from_mail.">\r\n";
+    $header .= "Reply-To: ".$replyto."\r\n";
+    $header .= "MIME-Version: 1.0\r\n";
+    $header .= "Content-Type: multipart/mixed; boundary=\"".$uid."\"\r\n\r\n";
+    $header .= "This is a multi-part message in MIME format.\r\n";
+    $header .= "--".$uid."\r\n";
+    $header .= "Content-type:text/plain; charset=iso-8859-1\r\n";
+    $header .= "Content-Transfer-Encoding: 7bit\r\n\r\n";
+    $header .= $message."\r\n\r\n";
+    $header .= "--".$uid."\r\n";
+    $header .= "Content-Type: application/octet-stream; name=\"".$filename."\"\r\n"; // use different content types here
+    $header .= "Content-Transfer-Encoding: base64\r\n";
+    $header .= "Content-Disposition: attachment; filename=\"".$filename."\"\r\n\r\n";
+    $header .= $content."\r\n\r\n";
+    $header .= "--".$uid."--";
+    if (mail($mailto, $subject, "", $header)) {
+        
+		$tb_name="sent_mail";
+		$id=$_SESSION['usr'];	
+
+		$query1="select email from members where username='$id'";
+
+		$raw_results=mysql_query($query1);
+		$results=mysql_fetch_array($raw_results);
+
+		$from=$results['email'];
+
+		$query2="   insert into $tb_name (id,sent_from,sent_to,msg_details,msg_path,sent_time)  
+					values ('$id','$from','$mailto','$subject','$message','$saved_file_path','$sent_time')";
+
+		if(mysql_query($query2))
+		{
+			header("location : Operator.html");
+		}
+		else 
+		{
+			header("location : Sendmail.html");
+		}
+		
+		
+			
+    } else {
+        header("location : Sendmail.html");
+    }
+}
+?>
